17bce8f1a862c4fbb982616adccac20f374d5263
sknutsen <sondreknutsen1@gmail.com>
2025-06-05 18:10:28 +0200

    pingu

---
+++ hosts/pingu/configuration.nix
@@ -0,0 +1,296 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+{
+  config,
+  pkgs,
+  ...
+}: {
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+  ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "pingu"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Bluetooth
+  hardware = {
+    bluetooth = {
+      enable = true;
+      powerOnBoot = true;
+      settings = {
+        General = {
+          Enable = "Source,Sink,Media,Socket";
+          Experimental = true;
+        };
+      };
+    };
+  };
+
+  # Set your time zone.
+  time.timeZone = "Europe/Oslo";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_GB.UTF-8";
+
+  # Configure console keymap
+  console.keyMap = "no";
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.zdk = {
+    useDefaultShell = true;
+    isNormalUser = true;
+    description = "Sondre Knutsen";
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+      "libvirtd"
+      "scanner"
+      "lp"
+      "video"
+      "input"
+      "audio"
+      "syncthing"
+    ];
+    packages = with pkgs; [
+      librewolf
+    ];
+  };
+
+  users.defaultUserShell = pkgs.zsh;
+
+  # Enable automatic login for the user.
+  services.getty.autologinUser = "zdk";
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    # System Packages
+    bc
+    baobab
+    btrfs-progs
+    clang
+    cmake
+    curl
+    cpufrequtils
+    duf
+    findutils
+    ffmpeg
+    fzf
+    glib #for gsettings to work
+    gsettings-qt
+    git
+    home-manager
+    keepassxc
+    killall
+    lazygit
+    libappindicator
+    libnotify
+    #neovim
+    openssl #required by Rainbow borders
+    pciutils
+    stow
+    wget
+    xdg-user-dirs
+    xdg-utils
+
+    # Hyprland
+    ags # desktop overview
+    btop
+    brightnessctl # for brightness control
+    cliphist
+    gtk-engine-murrine #for gtk themes
+    jq
+    kitty
+    networkmanagerapplet
+    nvtopPackages.full
+    pamixer
+    pavucontrol
+    playerctl
+    polkit_gnome
+    hypridle
+    libsForQt5.qt5ct
+    libsForQt5.qtstyleplugin-kvantum #kvantum
+    kdePackages.qt6ct
+    kdePackages.qtwayland
+    kdePackages.qtstyleplugin-kvantum #kvantum
+    rofi-wayland
+    swaynotificationcenter
+    swww
+    unzip
+    wallust
+    wl-clipboard
+    wlogout
+    xarchiver
+    yad
+  ];
+
+  services = {
+    xserver = {
+      enable = false;
+      xkb = {
+        layout = "no";
+        variant = "nodeadkeys";
+      };
+    };
+
+    greetd = {
+      enable = true;
+      vt = 3;
+      settings = {
+        default_session = {
+          user = "zdk";
+          command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd Hyprland"; # start Hyprland with a TUI login manager
+        };
+      };
+    };
+
+    smartd = {
+      enable = false;
+      autodetect = true;
+    };
+
+    gvfs.enable = true;
+    tumbler.enable = true;
+
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      wireplumber.enable = true;
+    };
+
+    udev.enable = true;
+    envfs.enable = true;
+    dbus.enable = true;
+
+    gnome.gnome-keyring.enable = true;
+
+    libinput.enable = true;
+
+    blueman.enable = true;
+
+    syncthing = {
+      enable = true;
+    };
+  };
+
+  # FONTS
+  fonts.packages = with pkgs; [
+    noto-fonts
+    fira-code
+    noto-fonts-cjk-sans
+    jetbrains-mono
+    font-awesome
+    terminus_font
+    victor-mono
+    (nerdfonts.override {fonts = ["JetBrainsMono"];}) # stable banch
+    (nerdfonts.override {fonts = ["FantasqueSansMono"];}) # stable banch
+
+    #nerd-fonts.jetbrains-mono # unstable
+    #nerd-fonts.fira-code # unstable
+    #nerd-fonts.fantasque-sans-mono #unstable
+  ];
+
+  programs = {
+    hyprland = {
+      enable = true;
+      withUWSM = true; # recommended for most users
+      xwayland.enable = true; # Xwayland can be disabled.
+    };
+
+    neovim = {
+      enable = true;
+    };
+
+    waybar.enable = true;
+    hyprlock.enable = true;
+
+    thunar.enable = true;
+    thunar.plugins = with pkgs.xfce; [
+      exo
+      mousepad
+      thunar-archive-plugin
+      thunar-volman
+      tumbler
+    ];
+
+    steam = {
+      enable = true;
+      gamescopeSession.enable = true;
+      remotePlay.openFirewall = true;
+      dedicatedServer.openFirewall = true;
+    };
+
+    xwayland.enable = true;
+
+    dconf.enable = true;
+    seahorse.enable = true;
+    fuse.userAllowOther = true;
+    mtr.enable = true;
+    gnupg.agent = {
+      enable = true;
+      enableSSHSupport = true;
+    };
+
+    zsh.enable = true;
+  };
+
+  # Extra Portal Configuration
+  xdg.portal = {
+    enable = true;
+    wlr.enable = false;
+    extraPortals = [
+      pkgs.xdg-desktop-portal-gtk
+    ];
+    configPackages = [
+      pkgs.xdg-desktop-portal-gtk
+      pkgs.xdg-desktop-portal
+    ];
+  };
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  nix.settings.experimental-features = ["nix-command" "flakes"];
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.11"; # Did you read the comment?
+}
