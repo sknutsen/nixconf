298906df5b953ee8f447389d776ea1b2e9d880fd
sknutsen <sondreknutsen1@gmail.com>
2025-08-08 19:48:17 +0200

    zen prefs

--- home/modules/browser/zen-browser/preferences.nix
+++ home/modules/browser/zen-browser/preferences.nix
@@ -5,5 +5,29 @@
   ...
 }: {
   programs.zen-browser.policies.Preferences = {
+    "zen.glance.enabled" = {
+      "Value" = false;
+      "Status" = "locked";
+    };
+    "zen.view.compact.hide-toolbar" = {
+      "Value" = true;
+      "Status" = "locked";
+    };
+    "zen.view.compact.should-enable-at-startup" = {
+      "Value" = false;
+      "Status" = "locked";
+    };
+    "zen.view.show-newtab-button-border-top" = {
+      "Value" = true;
+      "Status" = "locked";
+    };
+    "zen.view.show-newtab-button-top" = {
+      "Value" = false;
+      "Status" = "locked";
+    };
+    "zen.view.use-single-toolbar" = {
+      "Value" = false;
+      "Status" = "locked";
+    };
   };
 }
